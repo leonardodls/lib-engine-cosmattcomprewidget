99c19cac8a216196b7a6f92a37f593e5a1a333b1
saini-pawan <pawan.saini@comprotechnologies.com>
2019-08-30 19:03:00 +0530

    dev commit - somewhat stable ver 0.0.1

--- src/libs/libs-frontend-comprehensiveWidget/src/js/comprehensiveWidget.js
+++ src/libs/libs-frontend-comprehensiveWidget/src/js/comprehensiveWidget.js
@@ -2,7 +2,7 @@
 (function ($) {
   $.fn.comprehensiveWidget = function (id, options) {
     debugger;
-   
+
     $(this).empty();
     let widget = {
       iframeID: id,
@@ -18,8 +18,8 @@
 
     };
 
-    
-    
+
+
 
     let type, $container1, $container2, viewJSON, data;
     // let leoRightItem;
@@ -49,7 +49,7 @@
       if (type == 1) {
 
         //adding container
-        
+
         $div.append('<div class="fill-space"></div>');
         $container1 = $('<div id="container1" class="left-container sideBySide"></div>');
         $div.append($container1);
@@ -83,15 +83,21 @@
           widget.leoRightItem = setDataAndCreateGrids(data.rightSideData, $container2[0]);
           resizeGridContainers(false, $container1);
         } else {
-          
+
           widget.leoLeftItem = setDataAndCreateGrids(data.leftSideData, $container1[0]);
           widget.leoRightItem = setDataAndCreateGrids(data.rightSideData, $container2[0]);
-          
+          setUpContainers($container1);
         }
       }
       updateContainerItemProperties();
     };
 
+    let setUpContainers = function ($container1) {
+
+      $container1.css('padding', '0px');
+      $container1.css('background', 'none');
+
+    };
     let updateContainerItemProperties = function () {
       //temporarily removing
       return;
@@ -112,15 +118,15 @@
 
       let uiStyle = {
         height: height,
-        horizontalAlignment: 'center',
-        widgetStyles : "{'box-shadow': 'none', 'border': 'none', 'margin': '0px'}"
+        // horizontalAlignment: 'center',
+        widgetStyles: { 'box-shadow': 'none', 'border': 'none', 'margin': '0px' }
       };
 
       //wait for grid to load
       waitForEl(container, ".leonardoPlayerContainer", function (args) {
         args.css("overflow", "hidden");  // to hide its scrollbar
         waitForEl(args, ".l-act-player.presentation ", function (args1) {
-          args.css("margin-top", "-10px");  //to solve top mismatch issue
+          // args.css("margin-top", "-14px");  //to solve top mismatch issue
         });
       });
       if (gridData.height == "scroll") {
@@ -239,6 +245,9 @@
       });
       $container.on("minScreenEvent", function (event, args) {
         widget.isFullScreen = false;
+        //scrolling container position gets changed as by default window is scrolled , so scrolling back to top
+        $(window.parent).scrollTop(0);
+
         if (widget.leoRightItem && Object.keys(widget.leoRightItem).length === 0) {
           //abs
         } else {
@@ -252,93 +261,94 @@
       });
     };
     let resizeGridContainers = function (isFullScreen, scrollContainer) {
-try{
-  let ht  =$('.k-spreadsheet-view-size', $('#container1', $container) ).outerWidth(true);
-  if(ht!=undefined){
-    $('#container1', $container).css('max-width',ht + 50+  "px" );
-  }
-  let ht1  =$('.k-spreadsheet-view-size', $('#container2', $container) ).outerWidth(true);
-  if(ht1!=undefined){
-  $('#container2', $container).css('max-width',ht1 + 50+  "px" );
-  }
-      if (isFullScreen) {
-        //set height of container1 and container2 based on the height of bottom bar and top nav bar
-        let bottomBarHt = $('.bottomBar-cosmatengine').outerHeight(true);
-        let topBarHt = $('.topBar-cosmatengine').outerHeight(true);
-        let sum = bottomBarHt + topBarHt + 10;   //10px buffer
-        let height = window.parent.innerHeight - sum;
-        $('#container1', $container).css("height", height + "px");
-        $('#container2', $container).css("height", height + "px");
+      try {
+        let ht = $('.k-spreadsheet-view-size', $('#container1', $container)).outerWidth(true);
+        if (ht != undefined) {
+          $('#container1', $container).css('max-width', ht + 30 + "px");
+        }
+        let ht1 = $('.k-spreadsheet-view-size', $('#container2', $container)).outerWidth(true);
+        if (ht1 != undefined) {
+          //since it has padding present extra 20 px required
+          $('#container2', $container).css('max-width', ht1 + 50 + "px");
+        }
+        if (isFullScreen) {
+          //set height of container1 and container2 based on the height of bottom bar and top nav bar
+          let bottomBarHt = $('.bottomBar-cosmatengine').outerHeight(true);
+          let topBarHt = $('.topBar-cosmatengine').outerHeight(true);
+          let sum = bottomBarHt + topBarHt + 10;   //10px buffer
+          let height = window.parent.innerHeight - sum;
+          $('#container1', $container).css("height", height + "px");
+          $('#container2', $container).css("height", height + "px");
 
 
-        $('#placeholder').css('margin', '0');
-        $('#placeholder').removeClass('ribbon-adjustments');
-      } else {
+          $('#placeholder').css('margin', '0');
+          $('#placeholder').removeClass('ribbon-adjustments');
+        } else {
 
-        if (widget.hasHTML) {
-          // left container
-          if (widget.options.view.sidebyside.leftSideData.height == "scroll") {
-            setScrollContainerMS(scrollContainer);
-          } else {
-            let ht = $(widget.expandContainer).find('.html-viewer').outerHeight(true);
-            $(widget.expandContainer).css("height", ht + "px");
-          }
-
-          // right item will be there
-          if (widget.options.view.sidebyside.rightSideData.height == "scroll") {
-            if (widget.scrollingLeoItem && widget.leoRightItem == widget.expandLeoItem) {
+          if (widget.hasHTML) {
+            // left container
+            if (widget.options.view.sidebyside.leftSideData.height == "scroll") {
               setScrollContainerMS(scrollContainer);
+            } else {
+              let ht = $(widget.expandContainer).find('.html-viewer').outerHeight(true);
+              $(widget.expandContainer).css("height", ht + "px");
             }
-          } else {
-            setExpandContainerMS();
+
+            // right item will be there
+            if (widget.options.view.sidebyside.rightSideData.height == "scroll") {
+              if (widget.scrollingLeoItem && widget.leoRightItem == widget.expandLeoItem) {
+                setScrollContainerMS(scrollContainer);
+              }
+            } else {
+              setExpandContainerMS();
+            }
+            return;
           }
-          return;
+
+          setScrollContainerMS(scrollContainer);
+          setExpandContainerMS();
+
         }
 
-        setScrollContainerMS(scrollContainer);
-        setExpandContainerMS();
-
+      } catch (error) {
+        console.log(error);
       }
-
-    }catch(error){
-      console.log(error);
-          }
     };
 
     //MS stands for min screen
     let setScrollContainerMS = function (scrollContainer) {
-      try{
-      // set height of scroll container 
-      let bottomBarHt = $('.app-footer', $(window.parent.document)).outerHeight(true);
-      let topBarHt = $('.navbar', $(window.parent.document)).outerHeight(true);
-      let sum = bottomBarHt + topBarHt + 10;   //10px buffer
-      let height = window.parent.innerHeight - sum;
-      let gridHeight = -1;
+      try {
+        // set height of scroll container 
+        let bottomBarHt = $('.app-footer', $(window.parent.document)).outerHeight(true);
+        let topBarHt = $('.navbar', $(window.parent.document)).outerHeight(true);
+        let sum = bottomBarHt + topBarHt + 10;   //10px buffer
+        let height = window.parent.innerHeight - sum;
+        let gridHeight = -1;
 
-      // check container height should not be greater than the grid hieght 
+        // check container height should not be greater than the grid hieght 
 
 
-      if (widget.scrollingLeoItem && Object.keys(widget.scrollingLeoItem).length !== 0) {
-        gridHeight = widget.scrollingLeoItem.getRequiredDimension().height;
+        if (widget.scrollingLeoItem && Object.keys(widget.scrollingLeoItem).length !== 0) {
+          gridHeight = widget.scrollingLeoItem.getRequiredDimension().height;
+        }
+
+        // 8px buffer
+        let setheight = gridHeight + parseInt($(scrollContainer).find('.l-act-player').css('margin-bottom')) + 8 +
+          parseInt($(scrollContainer).css("padding-top")) + parseInt($(scrollContainer).css("padding-bottom"));
+
+        if (gridHeight !== -1 && setheight < height) {
+          debugger;
+          // let setheight = $(scrollContainer).find('.l-act-player').outerHeight(true) +
+          // parseInt($(scrollContainer).css("padding-top")) + parseInt($(scrollContainer).css("padding-bottom"));
+          // grid is smaller than viewport height, dont set absolute height
+          $(scrollContainer).css("height", setheight + "px");
+        } else {
+          $(scrollContainer).css("height", height + "px");
+        }
+
+      } catch (error) {
+        console.log(error);
       }
-
-      // 8px buffer
-      let setheight = gridHeight + parseInt($(scrollContainer).find('.l-act-player').css('margin-bottom')) + 8 +
-        parseInt($(scrollContainer).css("padding-top")) + parseInt($(scrollContainer).css("padding-bottom"));
-
-      if (gridHeight !== -1 && setheight < height) {
-        debugger;
-        // let setheight = $(scrollContainer).find('.l-act-player').outerHeight(true) +
-        // parseInt($(scrollContainer).css("padding-top")) + parseInt($(scrollContainer).css("padding-bottom"));
-        // grid is smaller than viewport height, dont set absolute height
-        $(scrollContainer).css("height", setheight + "px");
-      } else {
-        $(scrollContainer).css("height", height + "px");
-      }
-
-    }catch(error){
-      console.log(error);
-          }
     };
     //MS stands for min screen
     let setExpandContainerMS = function () {
@@ -350,32 +360,32 @@ try{
       }
     };
     window.parent.onscroll = function () {
-      try{
-      let iframeID = $('.pluginArea').data('widgetData').iframeID;
-      let iframeTop = $(window.parent.document).find('#' + 'iframe_' + iframeID).offset().top; //209
-      let navBarHt = $('.navbar', $(window.parent.document)).outerHeight(true);
-      let scrollingContainer = $('.pluginArea').data('widgetData').scrollingContainer;
+      try {
+        let iframeID = $('.pluginArea').data('widgetData').iframeID;
+        let iframeTop = $(window.parent.document).find('#' + 'iframe_' + iframeID).offset().top; //209
+        let navBarHt = $('.navbar', $(window.parent.document)).outerHeight(true);
+        let scrollingContainer = $('.pluginArea').data('widgetData').scrollingContainer;
 
-      let scrollingContainerHeight = $(scrollingContainer).outerHeight(true);
-      let iframeHeight = $(window.parent.document).find('#' + 'iframe_' + iframeID).outerHeight(true);
-      let bottomBarHeight = $('.bottomBar-cosmatengine').outerHeight(true);
+        let scrollingContainerHeight = $(scrollingContainer).outerHeight(true);
+        let iframeHeight = $(window.parent.document).find('#' + 'iframe_' + iframeID).outerHeight(true);
+        let bottomBarHeight = $('.bottomBar-cosmatengine').outerHeight(true);
 
-      // 17 px for scroll bar and 10 px padding bottom
-      let distFromTop = iframeTop + iframeHeight - scrollingContainerHeight - bottomBarHeight - 17;
-      if (window.parent.pageYOffset >= distFromTop) {
-        //set some top
-        $(scrollingContainer).animate({
-          top: distFromTop + "px"
-        }, 0, 'linear');
-      } else {
-        $(scrollingContainer).animate({
-          top: window.parent.pageYOffset - iframeTop + navBarHt + 0 + "px"
-        }, 0, 'linear');
+        // 17 px for scroll bar and 10 px padding bottom
+        let distFromTop = iframeTop + iframeHeight - scrollingContainerHeight - bottomBarHeight - 17;
+        if (window.parent.pageYOffset >= distFromTop) {
+          //set some top
+          $(scrollingContainer).animate({
+            top: distFromTop + "px"
+          }, 0, 'linear');
+        } else {
+          $(scrollingContainer).animate({
+            top: window.parent.pageYOffset - iframeTop + navBarHt + 0 + "px"
+          }, 0, 'linear');
+        }
+
+      } catch (error) {
+        console.log(error);
       }
-
-    }catch(error){
-      console.log(error);
-          }
     };
 
     window.parent.onresize = function () {
@@ -404,7 +414,7 @@ try{
       leoLeftItem: widget.leoLeftItem,
       leoRightItem: widget.leoRightItem
     };
-     
-   
+
+
   }
 })(jQuery);
